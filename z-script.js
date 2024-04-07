3730dfa3865560a0cbd8dd01f655612f458c9237
Kamavelli73 <ibrahimakamara180@gmail.com>
2024-04-07 10:52:22 +0000

    first commit

---
+++ z-script.js
@@ -0,0 +1,10 @@
+console.log('hello world')
+
+console.log(2+2) 
+
+let numbers = [19, 65, 1, 2, 6, 9, 9, 2, 1]
+let i=0
+for(i=0; i<numbers.length; i++)
+  if (numbers[i]%2 !==0) {
+    console.log(numbers[i])
+  }
